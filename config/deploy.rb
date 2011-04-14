dfe5f91c59f45a3c8faa7e4b0a0847052d43d43d
louisolivier <lbureau62@hotmail.com>
2011-04-13 23:10:20 -0400

    cap8

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,11 @@ role :db,  "173.255.233.171", :primary => true # This is where Rails migrations 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-#namespace :deploy do
- #  task :start do ; end
-  # task :stop do ; end
-   #task :restart, :roles => :app, :except => { :no_release => true } do
-    # run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   #end
- #end
+If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
